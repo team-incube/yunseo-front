36a0a6f55ca780e1a71a721c198af48570fdf1b4
정수진 <tnwlspearl@gmail.com>
2025-12-03 17:01:47 +0900

    feat : Trash svg 추가

---
+++ src/assets/Trash.jsx
@@ -0,0 +1,22 @@
+export default function Trash({ color = "#ef4444", size = 20, ...props }) {
+  return (
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      width={size} 
+      height={size} 
+      viewBox="0 0 24 24" 
+      fill="none" 
+      stroke={color} 
+      strokeWidth="2" 
+      strokeLinecap="round" 
+      strokeLinejoin="round"
+      {...props} 
+    >
+      <path d="M3 6h18"/>
+      <path d="M19 6v14c0 1.1-.9 2-2 2H7c-1.1 0-2-.9-2-2V6"/>
+      <path d="M8 6V4c0-1.1.9-2 2-2h4c1.1 0 2 .9 2 2v2"/>
+      <line x1="10" y1="11" x2="10" y2="17"/>
+      <line x1="14" y1="11" x2="14" y2="17"/>
+    </svg>
+  );
+}
